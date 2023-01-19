3437faab18c7ab1d80bfa9fae94f5163a8c9a76d
domains18 <gibsonsgibson88@gmail.com>
2023-01-19 20:55:10 +0300

    login ui

--- client/src/pages/UserName.js
+++ client/src/pages/UserName.js
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import avatar from '../assets/profile.png';
+import styles from '../styles/Username.module.css';
 const UserName = () => {
   return (
     <div className="container mx-auto">
-      <div className="flex justify-center items-center h-screen">
-        <div>
-        <div className="title flex flex-col items-center">
-          <span className="py-4 text-xl w-2/3 text-center text-gray-500">
-            Please enter your username to continue
-          </span>
+      <div className="flex h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Please enter your username to continue
+            </span>
           </div>
           <form className="py-1">
             <div className="profile flex justify-center py-4">
-              {/* <img src="" alt="" /> */}
+              <img src={avatar} className={styles.profileImage} alt="avatar" />
             </div>
-            <div className="text-box flex flex-col items-center gap-6">          <h4 className='text-5xl font-bold '>Hello Again</h4>
+            <div className="text-box flex flex-col items-center gap-6">
+              <h4 className='text-5xl font-bold '>Hello Again</h4>
 
-              <input type="text" placeholder='userName' />
-              <button type='submit'>LogIn</button>
+              <input type="text" placeholder='userName' className='border-4 py-2 rounded-lg mr-px' />
+              <button type='submit' className={styles.btn}>LogIn</button>
             </div>
 
             <div className="text-center py-">
