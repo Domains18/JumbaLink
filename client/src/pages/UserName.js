aa5629d4e71f20c61f51ed40b7e3b78ca9fce966
domains18 <gibsonsgibson88@gmail.com>
2023-01-19 19:51:55 +0300

    login ui

--- client/src/pages/UserName.js
+++ client/src/pages/UserName.js
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const UserName = () => {
   return (
-    <div>UserName</div>
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+        <div className="title flex flex-col items-center">
+          <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+            Please enter your username to continue
+          </span>
+          </div>
+          <form className="py-1">
+            <div className="profile flex justify-center py-4">
+              {/* <img src="" alt="" /> */}
+            </div>
+            <div className="text-box flex flex-col items-center gap-6">          <h4 className='text-5xl font-bold '>Hello Again</h4>
+
+              <input type="text" placeholder='userName' />
+              <button type='submit'>LogIn</button>
+            </div>
+
+            <div className="text-center py-">
+              <span className='text-blue-500'>No Account? <Link to="/register" className='text-red-500'> Register here</Link> </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
