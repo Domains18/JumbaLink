681c0c81b793b8b3e8ebf7e4bf7033ed74226106
domains18 <gibsonsgibson88@gmail.com>
2023-01-19 23:12:51 +0300

    recover ui

--- client/src/pages/Recovery.js
+++ client/src/pages/Recovery.js
@@ -23,19 +23,16 @@ const Recovery = () => {
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <span className="py-4 text-md w-2/3 text-center text-gray-500">
-              Enter the OTP to reset your password
+              Enter the 6 digit OTP to reset your password
             </span>
           </div>
           <form className="py-1" onSubmit={formik.handleSubmit}>
-            <div className="profile flex justify-center py-4">
-              <img src={avatar} className={styles.profileImage} alt="avatar" />
-            </div>
             <div className="text-box flex flex-col items-center gap-6">
               <input {...formik.getFieldProps('Password')} type="text" placeholder='enter the OTP' className={styles.input} />
-              <button type='submit' className={styles.btn}>Reset</button>
+              <button type='submit' className={styles.btn}>Recover</button>
             </div>
             <div className="text-center py-">
-              <span className='text-blue-500'>No account? <Link to="/reset" className='text-red-500'> Register here</Link> </span>
+              <span className='text-blue-500'>Didn't recieve?<Link to="/reset" className='text-red-500'> Resend</Link> </span>
             </div>
           </form>
         </div>
