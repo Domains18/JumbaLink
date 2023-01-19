29f64a34d2bb439af22fb3ac9870372c8fb2c3b8
domains18 <gibsonsgibson88@gmail.com>
2023-01-19 22:30:12 +0300

    password validation

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,26 +1,27 @@
 import { toast } from "react-hot-toast"
 function verifyUsername(error = {}, values) {
     if (!values.UserName || values.UserName.includes(" ")) {
-        error.UserName = toast.error('Missing or Invalid UserName')
+        error.UserName = toast.error('Missing or Invalid UserName');
     } 
     return error;
 }
 
 export async function validateUserName(values) {
-    const errors = verifyUsername({}, values)
+    const errors = verifyUsername({}, values);
 
-    return errors
+    return errors;
 }
 //  password
 function verifyPassword( error = {}, values){
     const specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
     if (!values.Password || values.Password == " ") {
-        error.Password = toast.error("Password cannot be empty")
+        error.Password = toast.error("Password cannot be empty");
     } 
     else if (values.Password < 4) {
-        error.Password = toast.error("Password Must be at least 4 characters")
+        error.Password = toast.error("Password Must be at least 4 characters");
     }
     else if(!specialChars.test(values.Password)) {
-        error.Password = toast.error("Password Must contain at least one special character")
+        error.Password = toast.error("Password Must contain at least one special character");
     }
+    return error;
 }
