949dd2ef885ef93f0528f3e65739b39c6f941946
domains18 <gibsonsgibson88@gmail.com>
2023-01-19 22:40:08 +0300

    password ui

--- client/src/pages/Password.js
+++ client/src/pages/Password.js
@@ -4,11 +4,7 @@ import avatar from '../assets/profile.png';
 import styles from '../styles/Username.module.css';
 import { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
-import { validateUserName, validatePassword } from '../helper/validate';
-
-
-
-
+import { validatePassword } from '../helper/validate';
 const Password = () => {
   const formik = useFormik({
     initialValues: {
@@ -17,11 +13,11 @@ const Password = () => {
     validate: validatePassword,
     validateOnBlur: false,
     validateOnChange: false,
-    onSubmit: async values => {console.log(values)}
+    onSubmit: async values => { console.log(values) }
   })
   return (
     <div className="container mx-auto">
-          <Toaster position='top-center' reverseOrder={false}></Toaster>
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
       <div className="flex justify-center items-center min-h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
@@ -34,11 +30,9 @@ const Password = () => {
               <img src={avatar} className={styles.profileImage} alt="avatar" />
             </div>
             <div className="text-box flex flex-col items-center gap-6">
-              <input {...formik.getFieldProps('Password')}type="password" placeholder='enter your password' className='border-4 py-2 rounded-lg  w-full' />
+              <input {...formik.getFieldProps('Password')} type="password" placeholder='enter your password' className='border-4 py-2 rounded-lg  w-full' />
               <button type='submit' className={styles.btn}>LogIn</button>
             </div>
-
-
             <div className="text-center py-">
               <span className='text-blue-500'>Forgot Password? <Link to="/reset" className='text-red-500'> Reset here</Link> </span>
             </div>
